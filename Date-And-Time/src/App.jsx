b22d53fb21732d009c8ac0b242e5c1e4e5bb240d
Kumar Poudyal <poudyal.kumar@gmail.com>
2024-08-25 23:00:06 +1000

    markup

--- Date-And-Time/src/App.jsx
+++ Date-And-Time/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
 import ClockHeading from './components/ClockHeading'
 import CurrentTime from './components/CurrentTime'
 
