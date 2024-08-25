845dfaa5244f62b6ba05693b07f74ccc9e2f9d0c
Kumar Poudyal <poudyal.kumar@gmail.com>
2024-08-26 00:29:05 +1000

    markup

---
+++ Date-And-Time/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/React-Projects/Date-And-Time/',
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Optional: Include global SCSS variables or mixins here
+        additionalData: '@import "src/styles/main.scss";'
+      },
+      build: {
+        sourcemap: false, // Disable source maps for CSS and JS in production
+      },
+    },
+  },
+});
