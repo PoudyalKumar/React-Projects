d09c48b9565b31b4e07a8ae9f1ced7e10c38ef41
Kumar Poudyal <poudyal.kumar@gmail.com>
2024-08-26 00:46:31 +1000

    markup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Projects/Date-And-Time/',
+  base: '/React-Projects',
   plugins: [react()],
   css: {
     preprocessorOptions: {
