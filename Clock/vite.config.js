1bab1f56c0707e9651b0e6c2f8b24581365ac9cf
Kumar Poudyal <poudyal.kumar@gmail.com>
2024-08-31 20:07:00 +1000

    Deploy latest build

---
+++ Clock/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/React-Projects/Clock",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Optional: Include global SCSS variables or mixins here
+        additionalData: '@import "src/styles/main.scss";',
+      },
+      build: {
+        sourcemap: false, // Disable source maps for CSS and JS in production
+      },
+    },
+  },
+});
