d9c20d8711710d0da9a89f695b93026598d359d2
Kumar Poudyal <poudyal.kumar@gmail.com>
2024-08-31 20:51:06 +1000

    config

--- Calculator/vite.config.js
+++ Calculator/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Projects/calculator",
+  base: "/React-Projects/Calculator",
   css: {
     preprocessorOptions: {
       scss: {
